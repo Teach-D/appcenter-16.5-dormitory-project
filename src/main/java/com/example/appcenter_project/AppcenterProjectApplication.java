38af0759cf22867a9fa210fd50876235443e248f
Teach-D <wkadht0619@gmail.com>
2025-03-28 17:44:58 +0900

    first commit

---
+++ src/main/java/com/example/appcenter_project/AppcenterProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.appcenter_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppcenterProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppcenterProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/appcenter_project/AppcenterProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appcenter_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppcenterProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
