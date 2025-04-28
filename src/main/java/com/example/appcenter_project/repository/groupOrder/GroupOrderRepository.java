71b112d54b77794b5112c839a23d2cc82269faa8
Teach-D <wkadht0619@gmail.com>
2025-04-28 21:08:32 +0900

    add: GroupOrderRepository findByGroupOrderChatRoom_id 메서드 추가

--- src/main/java/com/example/appcenter_project/repository/groupOrder/GroupOrderRepository.java
+++ src/main/java/com/example/appcenter_project/repository/groupOrder/GroupOrderRepository.java
@@ -4,5 +4,8 @@ import com.example.appcenter_project.entity.groupOrder.GroupOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.Optional;
+
 public interface GroupOrderRepository extends JpaRepository<GroupOrder, Long>, JpaSpecificationExecutor<GroupOrder> {
+    Optional<GroupOrder> findByGroupOrderChatRoom_id(Long id);
 }
