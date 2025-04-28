f4f3023fd3334a9402d8e1155316283e8510c765
Teach-D <wkadht0619@gmail.com>
2025-04-28 17:15:36 +0900

    ResponseGroupOrderChatDto unreadUserCount null시 0으로 대입

--- src/main/java/com/example/appcenter_project/dto/response/groupOrder/ResponseGroupOrderChatDto.java
+++ src/main/java/com/example/appcenter_project/dto/response/groupOrder/ResponseGroupOrderChatDto.java
@@ -1,5 +1,6 @@
 package com.example.appcenter_project.dto.response.groupOrder;
 
+import com.example.appcenter_project.entity.groupOrder.GroupOrderChat;
 import lombok.*;
 
 @Builder
@@ -12,4 +13,17 @@ public class ResponseGroupOrderChatDto {
     private Long groupOrderChatId;
     private String content;
     private Integer unreadUserCount;
+    private Long userId;
+
+    public static ResponseGroupOrderChatDto entityToDto(GroupOrderChat groupOrderChat) {
+        return ResponseGroupOrderChatDto.builder()
+                .groupOrderChatRoomId(groupOrderChat.getGroupOrderChatRoom().getId())
+                .groupOrderChatId(groupOrderChat.getId())
+                .content(groupOrderChat.getContent())
+                .unreadUserCount(
+                        groupOrderChat.getUnreadUser() != null ? groupOrderChat.getUnreadUser().size() : 0
+                )
+                .userId(groupOrderChat.getUser().getId())
+                .build();
+    }
 }
