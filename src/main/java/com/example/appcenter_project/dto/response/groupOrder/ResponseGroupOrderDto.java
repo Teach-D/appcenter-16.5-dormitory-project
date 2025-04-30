d668cc71d3c9019ceb50c03adab75c906acc848c
Teach-D <wkadht0619@gmail.com>
2025-04-30 19:17:36 +0900

    add: ResponseGroupOrderCommentDto 추가

--- src/main/java/com/example/appcenter_project/dto/response/groupOrder/ResponseGroupOrderDto.java
+++ src/main/java/com/example/appcenter_project/dto/response/groupOrder/ResponseGroupOrderDto.java
@@ -3,6 +3,9 @@ package com.example.appcenter_project.dto.response.groupOrder;
 import com.example.appcenter_project.entity.groupOrder.GroupOrder;
 import lombok.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
@@ -18,6 +21,7 @@ public class ResponseGroupOrderDto {
     private int groupOrderLike;
     private String description;
     private String link;
+    private List<ResponseGroupOrderCommentDto> groupOrderCommentDtoList = new ArrayList<>();
 
     public static ResponseGroupOrderDto entityToDto(GroupOrder groupOrder) {
         return ResponseGroupOrderDto.builder()
@@ -30,6 +34,21 @@ public class ResponseGroupOrderDto {
                 .groupOrderLike(groupOrder.getGroupOrderLike())
                 .description(groupOrder.getDescription())
                 .link(groupOrder.getLink())
+                .build();
+    }
+
+    public static ResponseGroupOrderDto detailEntityToDto(GroupOrder groupOrder, List<ResponseGroupOrderCommentDto> responseGroupOrderCommentDto) {
+        return ResponseGroupOrderDto.builder()
+                .groupOrderId(groupOrder.getId())
+                .title(groupOrder.getTitle())
+                .deadline(String.valueOf(groupOrder.getDeadline()))
+                .price(groupOrder.getPrice())
+                .currentPeople(groupOrder.getCurrentPeople())
+                .maxPeople(groupOrder.getMaxPeople())
+                .groupOrderLike(groupOrder.getGroupOrderLike())
+                .description(groupOrder.getDescription())
+                .link(groupOrder.getLink())
+                .groupOrderCommentDtoList(responseGroupOrderCommentDto)
                 .build();
     }
 }
