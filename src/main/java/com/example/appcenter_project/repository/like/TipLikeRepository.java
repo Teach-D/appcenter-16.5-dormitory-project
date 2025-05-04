0719be980ca96629b4b9d7fe70b279858df8b74b
Teach-D <wkadht0619@gmail.com>
2025-05-04 14:02:18 +0900

    feat: TipLike entity, repository 추가

---
+++ src/main/java/com/example/appcenter_project/repository/like/TipLikeRepository.java
@@ -0,0 +1,7 @@
+package com.example.appcenter_project.repository.like;
+
+import com.example.appcenter_project.entity.like.TipLike;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TipLikeRepository extends JpaRepository<TipLike, Long> {
+}
