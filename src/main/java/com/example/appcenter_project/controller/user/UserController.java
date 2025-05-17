f6b53276f138e6c2515cff171a6e12744029ebb2
Teach-D <wkadht0619@gmail.com>
2025-05-17 16:55:46 +0900

    feat: reissueAccessToken api 추가 #20

--- src/main/java/com/example/appcenter_project/controller/user/UserController.java
+++ src/main/java/com/example/appcenter_project/controller/user/UserController.java
@@ -6,19 +6,22 @@ import com.example.appcenter_project.dto.request.user.SignupUser;
 import com.example.appcenter_project.dto.response.like.ResponseLikeDto;
 import com.example.appcenter_project.dto.response.user.ResponseLoginDto;
 import com.example.appcenter_project.dto.response.user.ResponseUserDto;
-import com.example.appcenter_project.jwt.SecurityUser;
+import com.example.appcenter_project.security.CustomUserDetails;
 import com.example.appcenter_project.service.image.ImageService;
 import com.example.appcenter_project.service.user.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.Resource;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.Map;
 
 import static org.springframework.http.HttpStatus.*;
 
@@ -36,38 +39,48 @@ public class UserController {
         return ResponseEntity.status(CREATED).body(userService.saveUser(signupUser));
     }
 
+    @PostMapping("/refreshToken")
+    public ResponseEntity<?> reissueAccessToken(@RequestHeader("Authorization") String authorizationHeader) {
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            String refreshToken = authorizationHeader.substring(7);
+            String newAccessToken = userService.reissueAccessToken(refreshToken);
+            return ResponseEntity.ok(Map.of("accessToken", newAccessToken));
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Refresh Token이 유효하지 않습니다.");
+    }
+
     @GetMapping
-    public ResponseEntity<ResponseUserDto> findUserByUserId(@AuthenticationPrincipal SecurityUser user) {
-        return ResponseEntity.status(FOUND).body(userService.findUserByUserId(user.getId()));
+    public ResponseEntity<ResponseUserDto> findUserByUserId(@AuthenticationPrincipal CustomUserDetails user) {
+        return ResponseEntity.status(OK).body(userService.findUserByUserId(user.getId()));
     }
 
     @GetMapping("/image")
-    public ResponseEntity<Resource> findUserImageByUserId(@AuthenticationPrincipal SecurityUser user) {
-        ImageDto imageDto = imageService.findUserImageByUserId(user.getId());
+    public ResponseEntity<Resource> findUserImageByUserId(@AuthenticationPrincipal CustomUserDetails user) {
+        ImageDto imageDto = imageService.findUserImageByUserId(user.getUsername());
 
-        return ResponseEntity.status(FOUND)
+        return ResponseEntity.status(OK)
                 .contentType(MediaType.parseMediaType(imageDto.getContentType()))
                 .body(imageDto.getResource());
     }
 
     @GetMapping("/like")
-    public ResponseEntity<List<ResponseLikeDto>> findLikeByUserId(@AuthenticationPrincipal SecurityUser user) {
-        return ResponseEntity.status(FOUND).body(userService.findLikeByUserId(user.getId()));
+    public ResponseEntity<List<ResponseLikeDto>> findLikeByUserId(@AuthenticationPrincipal CustomUserDetails user) {
+        return ResponseEntity.status(OK).body(userService.findLikeByUserId(user.getId()));
     }
 
     @PutMapping
-    public ResponseEntity<ResponseUserDto> updateUser(@AuthenticationPrincipal SecurityUser user, @RequestBody RequestUserDto requestUserDto) {
-        return ResponseEntity.status(FOUND).body(userService.updateUser(user.getId(), requestUserDto));
+    public ResponseEntity<ResponseUserDto> updateUser(@AuthenticationPrincipal CustomUserDetails user, @RequestBody RequestUserDto requestUserDto) {
+        return ResponseEntity.status(OK).body(userService.updateUser(user.getId(), requestUserDto));
     }
 
     @PutMapping("/image")
-    public ResponseEntity<Void> updateUserImage(@AuthenticationPrincipal SecurityUser user, @RequestPart MultipartFile image) {
+    public ResponseEntity<Void> updateUserImage(@AuthenticationPrincipal CustomUserDetails user, @RequestPart MultipartFile image) {
         imageService.updateUserImage(user.getId(), image);
         return ResponseEntity.status(OK).build();
     }
 
     @DeleteMapping
-    public ResponseEntity<Void> deleteUser(@AuthenticationPrincipal SecurityUser user) {
+    public ResponseEntity<Void> deleteUser(@AuthenticationPrincipal CustomUserDetails user) {
         userService.deleteUser(user.getId());
         return ResponseEntity.status(NO_CONTENT).build();
     }
