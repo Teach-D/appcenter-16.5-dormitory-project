37ae5a428fe9ecf2459f3d9b573f51623c03bf86
Teach-D <wkadht0619@gmail.com>
2025-05-18 16:45:04 +0900

    refactor: 오타 수정

--- src/main/java/com/example/appcenter_project/dto/request/groupOrder/RequestGroupOrderChatDto.java
+++ src/main/java/com/example/appcenter_project/dto/request/groupOrder/RequestGroupOrderChatDto.java
@@ -1,11 +1,11 @@
 package com.example.appcenter_project.dto.request.groupOrder;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 public class RequestGroupOrderChatDto {
 
-    private Long userId;
-    private Long groupOrderChatRomId;
+    private Long groupOrderChatRoomId;
     private String content;
 }
