449aab0874fe6be6f063d49d72cf0788db9e6b3e
Teach-D <wkadht0619@gmail.com>
2025-05-19 11:53:10 +0900

    add: IMAGE_NOT_FOUND error code 추가 #41

--- src/main/java/com/example/appcenter_project/exception/ErrorCode.java
+++ src/main/java/com/example/appcenter_project/exception/ErrorCode.java
@@ -30,13 +30,15 @@ public enum ErrorCode {
     GROUP_ORDER_COMMENT_NOT_FOUND(NOT_FOUND, 3004, "[GroupOrder] 댓글을 찾을 수 없습니다."),
     GROUP_ORDER_TITLE_DUPLICATE(CONFLICT, 3005, "[GroupOrder] 이미 존재하는 제목입니다."),
 
-
     // TIP
     TIP_NOT_FOUND(NOT_FOUND, 4001, "[Tip] 팁 게시글을 찾을 수 없습니다."),
     TIP_COMMENT_NOT_FOUND(NOT_FOUND, 4002, "[Tip] 팁 게시글의 댓글을 찾을 수 없습니다."),
 
     // VALIDATION
-    VALIDATION_FAILED(BAD_REQUEST, 5001, "[Validation] DTO에서 요청한 값이 올바르지 않습니다.");
+    VALIDATION_FAILED(BAD_REQUEST, 5001, "[Validation] DTO에서 요청한 값이 올바르지 않습니다."),
+
+    // IMAGE
+    IMAGE_NOT_FOUND(NOT_FOUND, 6001, "[Image] 이미지를 찾을 수 없습니다.");
 
     private final HttpStatus httpStatus;
     private final Integer code;
