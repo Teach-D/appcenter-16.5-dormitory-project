b6126bdfdee2e3f5ab857e222a105ad0177b5b57
Teach-D <wkadht0619@gmail.com>
2025-05-20 21:11:20 +0900

    add: TIP_COMMENT_NOT_OWNED_BY_USER ErrorCode 추가 #41

--- src/main/java/com/example/appcenter_project/exception/ErrorCode.java
+++ src/main/java/com/example/appcenter_project/exception/ErrorCode.java
@@ -35,7 +35,7 @@ public enum ErrorCode {
     // TIP
     TIP_NOT_FOUND(NOT_FOUND, 4001, "[Tip] 팁 게시글을 찾을 수 없습니다."),
     TIP_COMMENT_NOT_FOUND(NOT_FOUND, 4002, "[Tip] 팁 게시글의 댓글을 찾을 수 없습니다."),
-
+    TIP_COMMENT_NOT_OWNED_BY_USER(FORBIDDEN, 4004, "[Tip] 팁 게시글의 댓글을 생성한 유저가 아니기 때문에 수정 및 삭제할 권한이 없습니다."),
     // VALIDATION
     VALIDATION_FAILED(BAD_REQUEST, 5001, "[Validation] DTO에서 요청한 값이 올바르지 않습니다."),
 
