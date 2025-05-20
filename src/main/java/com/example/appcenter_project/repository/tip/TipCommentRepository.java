cd34a302e41b48e20caad72f5f969f6d0eecdf73
Teach-D <wkadht0619@gmail.com>
2025-05-20 21:11:02 +0900

    add: findByIdAndUserId 메서드 추가 #41

--- src/main/java/com/example/appcenter_project/repository/tip/TipCommentRepository.java
+++ src/main/java/com/example/appcenter_project/repository/tip/TipCommentRepository.java
@@ -4,8 +4,10 @@ import com.example.appcenter_project.entity.tip.TipComment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface TipCommentRepository extends JpaRepository<TipComment, Long> {
     List<TipComment> findByTip_IdAndParentTipCommentIsNull(Long tipId);
     List<TipComment> findByTip_Id(Long tipId);
+    Optional<TipComment> findByIdAndUserId(Long id, Long userId);
 }
