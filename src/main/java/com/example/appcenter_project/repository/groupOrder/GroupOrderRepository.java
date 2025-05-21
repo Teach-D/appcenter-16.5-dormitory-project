da89817ac362ab913d8b04c982d5deef5e94a15a
Teach-D <wkadht0619@gmail.com>
2025-05-21 10:37:36 +0900

    add: findByIdAndUserId 메서드 추가 #41

--- src/main/java/com/example/appcenter_project/repository/groupOrder/GroupOrderRepository.java
+++ src/main/java/com/example/appcenter_project/repository/groupOrder/GroupOrderRepository.java
@@ -9,4 +9,5 @@ import java.util.Optional;
 public interface GroupOrderRepository extends JpaRepository<GroupOrder, Long>, JpaSpecificationExecutor<GroupOrder> {
     Optional<GroupOrder> findByGroupOrderChatRoom_id(Long id);
     boolean existsByTitle(String title);
+    Optional<GroupOrder> findByIdAndUserId(Long id, Long userId);
 }
