c65ccbe49db5a07f704f2ed674059d7c6ab1907b
Teach-D <wkadht0619@gmail.com>
2025-05-21 10:45:17 +0900

    fix: GroupOrderComment 삭제 시 User검증 #41

--- src/main/java/com/example/appcenter_project/controller/groupOrder/GroupOrderCommentController.java
+++ src/main/java/com/example/appcenter_project/controller/groupOrder/GroupOrderCommentController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 
 import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.NO_CONTENT;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,6 +25,12 @@ public class GroupOrderCommentController {
     @PostMapping
     public ResponseEntity<ResponseGroupOrderCommentDto> saveGroupOrderComment(@AuthenticationPrincipal CustomUserDetails user, @Valid @RequestBody RequestGroupOrderCommentDto requestGroupOrderCommentDto) {
         return ResponseEntity.status(CREATED).body(groupOrderCommentService.saveGroupOrderComment(user.getId(), requestGroupOrderCommentDto));
+    }
+
+    @DeleteMapping("/{groupOrderCommentId}")
+    public ResponseEntity<Void> deleteGroupOrderComment(@AuthenticationPrincipal CustomUserDetails user, @PathVariable Long groupOrderCommentId) {
+        groupOrderCommentService.deleteGroupOrderComment(user.getId(), groupOrderCommentId);
+        return ResponseEntity.status(NO_CONTENT).build();
     }
 
 }
