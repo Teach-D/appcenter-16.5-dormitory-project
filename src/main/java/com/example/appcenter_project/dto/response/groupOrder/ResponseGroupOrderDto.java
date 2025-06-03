d5ee2995cf84c5269102014fdbd6a7af8ea4972b
Teach-D <wkadht0619@gmail.com>
2025-06-03 20:44:10 +0900

    add: User의 게시글 조회를 위한 DTO 추상화 #71

--- src/main/java/com/example/appcenter_project/dto/response/groupOrder/ResponseGroupOrderDto.java
+++ src/main/java/com/example/appcenter_project/dto/response/groupOrder/ResponseGroupOrderDto.java
@@ -1,29 +1,38 @@
 package com.example.appcenter_project.dto.response.groupOrder;
 
+import com.example.appcenter_project.dto.response.user.ResponseBoardDto;
 import com.example.appcenter_project.entity.groupOrder.GroupOrder;
 import lombok.*;
 
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor(access = AccessLevel.PROTECTED)
-@Getter
-public class ResponseGroupOrderDto {
+import java.time.LocalDateTime;
 
-    private Long groupOrderId;
-    private String title;
+@Getter
+public class ResponseGroupOrderDto extends ResponseBoardDto {
+
     private String deadline;
     private int price;
     private int currentPeople;
     private int maxPeople;
 
+    @Builder
+    public ResponseGroupOrderDto(Long boardId, String title, String deadline, int price, int currentPeople, int maxPeople, LocalDateTime createTime, String type) {
+        super(boardId, title, type, createTime);
+        this.deadline = deadline;
+        this.price = price;
+        this.currentPeople = currentPeople;
+        this.maxPeople = maxPeople;
+    }
+
     public static ResponseGroupOrderDto entityToDto(GroupOrder groupOrder) {
         return ResponseGroupOrderDto.builder()
-                .groupOrderId(groupOrder.getId())
+                .boardId(groupOrder.getId())
                 .title(groupOrder.getTitle())
+                .type("GROUP_ORDER")
                 .deadline(String.valueOf(groupOrder.getDeadline()))
                 .price(groupOrder.getPrice())
                 .currentPeople(groupOrder.getCurrentPeople())
                 .maxPeople(groupOrder.getMaxPeople())
+                .createTime(groupOrder.getCreatedDate())
                 .build();
 
     }
--- src/main/java/com/example/appcenter_project/dto/response/tip/ResponseTipDto.java
+++ src/main/java/com/example/appcenter_project/dto/response/tip/ResponseTipDto.java
@@ -1,32 +1,39 @@
 package com.example.appcenter_project.dto.response.tip;
 
+import com.example.appcenter_project.dto.response.user.ResponseBoardDto;
 import com.example.appcenter_project.entity.tip.Tip;
 import lombok.*;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-public class ResponseTipDto {
+public class ResponseTipDto extends ResponseBoardDto {
 
-    private String title;
     private String content;
     private Integer tipLikeCount;
     private Integer tipCommentCount;
-    private String createTime;
+
+    @Builder
+    public ResponseTipDto(Long boardId, String title, String content, Integer tipLikeCount, Integer tipCommentCount, LocalDateTime createTime, String type) {
+        super(boardId, title, type, createTime);
+        this.content = content;
+        this.tipLikeCount = tipLikeCount;
+        this.tipCommentCount = tipCommentCount;
+    }
 
     public static ResponseTipDto entityToDto(Tip tip) {
         return ResponseTipDto.builder()
+                .boardId(tip.getId())
                 .title(tip.getTitle())
+                .type("TIP")
                 .content(tip.getContent())
                 .tipLikeCount(tip.getTipLike())
                 .tipCommentCount(
                         tip.getTipCommentList() != null ? tip.getTipCommentList().size() : 0
                 )
-                .createTime(String.valueOf(tip.getCreatedDate()))
+                .createTime(tip.getCreatedDate())
                 .build();
     }
 }
