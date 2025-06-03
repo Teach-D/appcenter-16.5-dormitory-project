c32287fc374bd9b0a499932c2dd820c7f4cff257
Teach-D <wkadht0619@gmail.com>
2025-06-03 17:04:50 +0900

    add: findByUserAndGroupOrder 메서드 추가 #69

--- src/main/java/com/example/appcenter_project/repository/like/GroupOrderLikeRepository.java
+++ src/main/java/com/example/appcenter_project/repository/like/GroupOrderLikeRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
-public interface LikeRepository extends JpaRepository<GroupOrderLike, Long> {
+public interface GroupOrderLikeRepository extends JpaRepository<GroupOrderLike, Long> {
     List<GroupOrderLike> findByUser_Id(Long userId);
     boolean existsByUserAndGroupOrder(User user, GroupOrder groupOrder);
     Optional<GroupOrderLike> findByUserAndGroupOrder(User user, GroupOrder groupOrder);
