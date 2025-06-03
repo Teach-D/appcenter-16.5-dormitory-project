5354a6f3302ee56bc324d7b9eef3046ccf4d5bbc
Teach-D <wkadht0619@gmail.com>
2025-06-03 17:03:06 +0900

    add: GroupOrderLike와 양방향 매핑 #69

--- src/main/java/com/example/appcenter_project/repository/like/GroupOrderLikeRepository.java
+++ src/main/java/com/example/appcenter_project/repository/like/GroupOrderLikeRepository.java
@@ -1,10 +1,15 @@
 package com.example.appcenter_project.repository.like;
 
+import com.example.appcenter_project.entity.groupOrder.GroupOrder;
 import com.example.appcenter_project.entity.like.GroupOrderLike;
+import com.example.appcenter_project.entity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface LikeRepository extends JpaRepository<GroupOrderLike, Long> {
     List<GroupOrderLike> findByUser_Id(Long userId);
+    boolean existsByUserAndGroupOrder(User user, GroupOrder groupOrder);
+    Optional<GroupOrderLike> findByUserAndGroupOrder(User user, GroupOrder groupOrder);
 }
