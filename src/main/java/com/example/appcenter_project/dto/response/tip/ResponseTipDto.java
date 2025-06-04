a4585278afb259317b2e937e589c092121cf51e8
Teach-D <wkadht0619@gmail.com>
2025-06-04 19:40:13 +0900

    add: User의 게시글 조회를 위한 DTO 추상화 #71

--- src/main/java/com/example/appcenter_project/dto/response/tip/ResponseTipDto.java
+++ src/main/java/com/example/appcenter_project/dto/response/tip/ResponseTipDto.java
@@ -1,32 +1,64 @@
 package com.example.appcenter_project.dto.response.tip;
 
+import com.example.appcenter_project.dto.response.user.ResponseBoardDto;
+import com.example.appcenter_project.entity.Image;
 import com.example.appcenter_project.entity.tip.Tip;
 import lombok.*;
 
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-public class ResponseTipDto {
+public class ResponseTipDto extends ResponseBoardDto {
 
-    private String title;
     private String content;
     private Integer tipLikeCount;
     private Integer tipCommentCount;
-    private String createTime;
+
+    @Builder
+    public ResponseTipDto(Long boardId, String title, String content, Integer tipLikeCount, Integer tipCommentCount,
+                          LocalDateTime createTime, String type, String fileName) {
+        super(boardId, title, type, createTime, fileName);
+        this.content = content;
+        this.tipLikeCount = tipLikeCount;
+        this.tipCommentCount = tipCommentCount;
+    }
 
     public static ResponseTipDto entityToDto(Tip tip) {
+        Image image = tip.getImageList().get(0);
+        String fullPath = image.getFilePath();
+        String fileName = extractFileName(fullPath);
+
         return ResponseTipDto.builder()
+                .boardId(tip.getId())
                 .title(tip.getTitle())
+                .type("TIP")
                 .content(tip.getContent())
                 .tipLikeCount(tip.getTipLike())
                 .tipCommentCount(
                         tip.getTipCommentList() != null ? tip.getTipCommentList().size() : 0
                 )
-                .createTime(String.valueOf(tip.getCreatedDate()))
+                .createTime(tip.getCreatedDate())
+                .fileName(fileName)
                 .build();
+    }
+
+    private static String extractFileName(String fullPath) {
+        String markerWin = "tip\\";
+        String markerUnix = "tip/";
+
+        int index = fullPath.lastIndexOf(markerWin);
+        if (index != -1) {
+            return fullPath.substring(index + markerWin.length());
+        }
+
+        index = fullPath.lastIndexOf(markerUnix);
+        if (index != -1) {
+            return fullPath.substring(index + markerUnix.length());
+        }
+
+        return Paths.get(fullPath).getFileName().toString(); // fallback
     }
 }
