6e8a601486edea527b9aa320b61c35e6e2b44e08
Teach-D <wkadht0619@gmail.com>
2025-06-04 20:43:39 +0900

    add: User의 게시글 조회를 위한 DTO 추상화 #71

--- src/main/java/com/example/appcenter_project/dto/response/groupOrder/ResponseGroupOrderDto.java
+++ src/main/java/com/example/appcenter_project/dto/response/groupOrder/ResponseGroupOrderDto.java
@@ -1,30 +1,60 @@
 package com.example.appcenter_project.dto.response.groupOrder;
 
+import com.example.appcenter_project.dto.response.user.ResponseBoardDto;
 import com.example.appcenter_project.entity.groupOrder.GroupOrder;
 import lombok.*;
 
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor(access = AccessLevel.PROTECTED)
-@Getter
-public class ResponseGroupOrderDto {
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
 
-    private Long groupOrderId;
-    private String title;
+@Getter
+public class ResponseGroupOrderDto extends ResponseBoardDto {
+
     private String deadline;
     private int price;
     private int currentPeople;
     private int maxPeople;
 
+    @Builder
+    public ResponseGroupOrderDto(Long boardId, String title, String deadline, int price, int currentPeople, int maxPeople, LocalDateTime createTime, String type, String fileName) {
+        super(boardId, title, type, createTime,fileName);
+        this.deadline = deadline;
+        this.price = price;
+        this.currentPeople = currentPeople;
+        this.maxPeople = maxPeople;
+    }
+
     public static ResponseGroupOrderDto entityToDto(GroupOrder groupOrder) {
+        String fullPath = groupOrder.getImageList().get(0).getFilePath();
+        String fileName = extractFileName(fullPath);
+
         return ResponseGroupOrderDto.builder()
-                .groupOrderId(groupOrder.getId())
+                .boardId(groupOrder.getId())
                 .title(groupOrder.getTitle())
+                .type("GROUP_ORDER")
                 .deadline(String.valueOf(groupOrder.getDeadline()))
                 .price(groupOrder.getPrice())
                 .currentPeople(groupOrder.getCurrentPeople())
                 .maxPeople(groupOrder.getMaxPeople())
+                .createTime(groupOrder.getCreatedDate())
+                .fileName(fileName)
                 .build();
+    }
 
+    private static String extractFileName(String fullPath) {
+        String markerWin = "group-order\\";
+        String markerUnix = "group-order/";
+
+        int index = fullPath.lastIndexOf(markerWin);
+        if (index != -1) {
+            return fullPath.substring(index + markerWin.length());
+        }
+
+        index = fullPath.lastIndexOf(markerUnix);
+        if (index != -1) {
+            return fullPath.substring(index + markerUnix.length());
+        }
+
+        return Paths.get(fullPath).getFileName().toString(); // fallback
     }
 }
---
+++ src/main/java/com/example/appcenter_project/dto/response/user/ResponseBoardDto.java
@@ -0,0 +1,17 @@
+package com.example.appcenter_project.dto.response.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@Getter
+public abstract class ResponseBoardDto {
+
+    private Long boardId;
+    private String title;
+    private String type;
+    private LocalDateTime createTime;
+    private String fileName;
+}
