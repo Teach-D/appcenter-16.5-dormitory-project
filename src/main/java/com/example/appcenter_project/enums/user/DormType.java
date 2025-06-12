71e912918862d4110890039f1ba7195e04d97f69
Jimin15 <sujean07015@gmail.com>
2025-06-13 04:37:48 +0900

    feat: DormType enum에 @JsonCreator, @JsonValue 추가하여 한글 직렬화/역직렬화 지원 #17

--- src/main/java/com/example/appcenter_project/enums/user/DormType.java
+++ src/main/java/com/example/appcenter_project/enums/user/DormType.java
@@ -1,5 +1,7 @@
 package com.example.appcenter_project.enums.user;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -10,4 +12,19 @@ public enum DormType {
     DORM_3("3기숙사");
 
     private final String description;
+
+    @JsonCreator
+    public static DormType from(String value) {
+        for (DormType type : DormType.values()) {
+            if (type.getDescription().equals(value)) {
+                return type;
+            }
+        }
+        throw new IllegalArgumentException("Invalid DormType: " + value);
+    }
+
+    @JsonValue
+    public String toValue() {
+        return this.description;
+    }
 }
