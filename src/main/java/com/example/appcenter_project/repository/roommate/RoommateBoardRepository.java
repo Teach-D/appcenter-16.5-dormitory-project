869206e69a118ce980f87a64aaf66ff4656212f9
Jimin15 <sujean07015@gmail.com>
2025-06-13 04:39:57 +0900

    feat: RoommateBoard 엔티티용 JPA 레포지토리 인터페이스 추가 #17

---
+++ src/main/java/com/example/appcenter_project/repository/roommate/RoommateBoardRepository.java
@@ -0,0 +1,7 @@
+package com.example.appcenter_project.repository.roommate;
+
+import com.example.appcenter_project.entity.roommate.RoommateBoard;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoommateBoardRepository extends JpaRepository<RoommateBoard, Long> {
+}
