8422b4d46eedd2eb3865ac8f64f072259ea53483
Jimin15 <sujean07015@gmail.com>
2025-06-13 05:24:07 +0900

    [feat] RoommateBoardRepository에 최신순 정렬 조회 기능 추가 #87

--- src/main/java/com/example/appcenter_project/repository/roommate/RoommateBoardRepository.java
+++ src/main/java/com/example/appcenter_project/repository/roommate/RoommateBoardRepository.java
@@ -3,5 +3,8 @@ package com.example.appcenter_project.repository.roommate;
 import com.example.appcenter_project.entity.roommate.RoommateBoard;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RoommateBoardRepository extends JpaRepository<RoommateBoard, Long> {
+    List<RoommateBoard> findAllByOrderByCreatedDateDesc();
 }
