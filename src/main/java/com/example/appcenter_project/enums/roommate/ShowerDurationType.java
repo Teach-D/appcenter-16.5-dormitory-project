23e8362fb42b89487db76c942407ccdc8f252e8f
Jimin15 <sujean07015@gmail.com>
2025-06-13 04:35:27 +0900

    feat: ShowerDurationType enum 클래스 생성 및 한글 설명 직렬화/역직렬화 처리 #17

---
+++ src/main/java/com/example/appcenter_project/enums/roommate/ShowerDurationType.java
@@ -0,0 +1,31 @@
+package com.example.appcenter_project.enums.roommate;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum ShowerDurationType {
+    WITHIN_10_MINUTES("10분 이내"),
+    WITHIN_30_MINUTES("30분 이내"),
+    WITHIN_1_HOUR("1시간 이내");
+
+    private final String description;
+
+    @JsonCreator
+    public static ShowerDurationType from(String value) {
+        for (ShowerDurationType type : ShowerDurationType.values()) {
+            if (type.getDescription().equals(value)) {
+                return type;
+            }
+        }
+        throw new IllegalArgumentException("Invalid ShowerDurationType: " + value);
+    }
+
+    @JsonValue
+    public String toValue() {
+        return this.description;
+    }
+}
