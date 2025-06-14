75030b2afd921bdcb8113301dd43585db98d6099
Jimin15 <sujean07015@gmail.com>
2025-06-14 14:12:34 +0900

    refactor: Roommate 게시글 작성 시 @AuthenticationPrincipal 기반 인증 사용자 정보 사용으로 변경 #17

--- src/main/java/com/example/appcenter_project/controller/roommate/RoommateController.java
+++ src/main/java/com/example/appcenter_project/controller/roommate/RoommateController.java
@@ -2,15 +2,20 @@ package com.example.appcenter_project.controller.roommate;
 
 import com.example.appcenter_project.dto.request.roommate.RequestRoommateFormDto;
 import com.example.appcenter_project.dto.response.roommate.ResponseRoommatePostDto;
+import com.example.appcenter_project.entity.roommate.RoommateBoard;
+import com.example.appcenter_project.security.CustomUserDetails;
 import com.example.appcenter_project.service.roommate.RoommateService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/api/roommate")
+@RequestMapping("/roommate")
 @RequiredArgsConstructor
 public class RoommateController {
 
@@ -18,9 +23,10 @@ public class RoommateController {
 
     @PostMapping("/post")
     public ResponseEntity<ResponseRoommatePostDto> createRoommatePost(
-            @RequestParam Long userId,
+            @AuthenticationPrincipal CustomUserDetails userDetails,
             @RequestBody RequestRoommateFormDto requestDto
     ) {
+        Long userId = userDetails.getId(); // 인증된 사용자 ID 가져오기
         ResponseRoommatePostDto responseDto = roommateService.createRoommateCheckListandBoard(requestDto, userId);
         return ResponseEntity.ok(responseDto);
     }
