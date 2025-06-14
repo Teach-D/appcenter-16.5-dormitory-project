1970e1384bd5533e88d0b6fa9c8a16b4b38c11f4
Jimin15 <sujean07015@gmail.com>
2025-06-14 21:36:09 +0900

    feat: RoommateBoardRepository 인터페이스 생성 #87

--- src/main/java/com/example/appcenter_project/repository/roommate/RoommateBoardRepository.java
+++ src/main/java/com/example/appcenter_project/repository/roommate/RoommateBoardRepository.java
@@ -3,5 +3,11 @@ package com.example.appcenter_project.repository.roommate;
 import com.example.appcenter_project.entity.roommate.RoommateBoard;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface RoommateBoardRepository extends JpaRepository<RoommateBoard, Long> {
+    List<RoommateBoard> findAllByOrderByCreatedDateDesc();
+    Optional<RoommateBoard> findByUserId(Long userId);
+
 }
