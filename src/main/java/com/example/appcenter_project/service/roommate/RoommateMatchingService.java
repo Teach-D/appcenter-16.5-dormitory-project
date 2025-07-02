b4ec8d585b8e362d4aa2e5c806d765e3fd3e4543
Jimin15 <sujean07015@gmail.com>
2025-07-02 21:43:07 +0900

    feat: 룸메이트 매칭 수락 기능 추가 #107

--- src/main/java/com/example/appcenter_project/service/roommate/RoommateMatchingService.java
+++ src/main/java/com/example/appcenter_project/service/roommate/RoommateMatchingService.java
@@ -52,4 +52,37 @@ public class RoommateMatchingService {
                 .build();
     }
 
+    // 매칭 수락
+    public void acceptMatching(Long matchingId, Long userId) {
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new CustomException(ErrorCode.ROOMMATE_USER_NOT_FOUND));
+
+        RoommateMatching matching = roommateMatchingRepository.findById(matchingId)
+                .orElseThrow(() -> new CustomException(ErrorCode.ROOMMATE_MATCHING_NOT_FOUND));
+
+
+        // 매칭 요청의 수신자가 현재 로그인한 사용자인지 확인
+        if (!matching.getReceiver().getId().equals(user.getId())) {
+            throw new CustomException(ErrorCode.ROOMMATE_MATCHING_NOT_FOR_USER);
+        }
+
+
+        // 이미 완료된 요청인지 확인
+        if (matching.getStatus() != MatchingStatus.REQUEST) {
+            throw new CustomException(ErrorCode.ROOMMATE_MATCHING_ALREADY_COMPLETED);
+        }
+
+        // 이미 매칭 완료된 사람인지 확인
+        if (user.getRoommateBoard() != null) {
+            throw new CustomException(ErrorCode.ROOMMATE_ALREADY_MATCHED);
+        }
+
+        matching.complete();
+    }
+
+
+
+
+
 }
