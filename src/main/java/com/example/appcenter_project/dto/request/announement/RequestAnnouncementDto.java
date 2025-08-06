81a6519245379c14bd3c01347d1e3fefa9ed2860
Teach-D <wkadht0619@gmail.com>
2025-08-06 17:13:48 +0900

    feat: announcement api 개발 #168

---
+++ src/main/java/com/example/appcenter_project/dto/request/announement/RequestAnnouncementDto.java
@@ -0,0 +1,23 @@
+package com.example.appcenter_project.dto.request.announement;
+
+import com.example.appcenter_project.entity.announcement.Announcement;
+import lombok.Getter;
+
+@Getter
+public class RequestAnnouncementDto {
+
+    private String title;
+    private String writer;
+    private String content;
+    private Boolean isEmergency;
+
+    public static Announcement dtoToEntity(RequestAnnouncementDto requestAnnouncementDto) {
+        return Announcement.builder()
+                .title(requestAnnouncementDto.getTitle())
+                .writer(requestAnnouncementDto.getWriter())
+                .content(requestAnnouncementDto.getContent())
+                .isEmergency(requestAnnouncementDto.getIsEmergency())
+                .build();
+    }
+
+}
