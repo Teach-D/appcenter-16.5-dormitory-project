f0387293e93a43c490a61748ef7691313d91e353
Jimin15 <sujean07015@gmail.com>
2025-08-13 18:42:18 +0900

    feat: RoommateBoardRepository에 페이지네이션 및 유사도 계산용 조회 메서드 추가 #249

--- src/main/java/com/example/appcenter_project/repository/roommate/RoommateBoardRepository.java
+++ src/main/java/com/example/appcenter_project/repository/roommate/RoommateBoardRepository.java
@@ -1,6 +1,7 @@
 package com.example.appcenter_project.repository.roommate;
 
 import com.example.appcenter_project.entity.roommate.RoommateBoard;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -12,5 +13,11 @@ public interface RoommateBoardRepository extends JpaRepository<RoommateBoard, Lo
 
     // 가장 최근 10개의 게시글 조회
     List<RoommateBoard> findTop10ByOrderByCreatedDateDesc();
+
+    List<RoommateBoard> findAllByOrderByIdDesc(Pageable pageable);
+
+    List<RoommateBoard> findByIdLessThanOrderByIdDesc(Long lastId, Pageable pageable);
+    // 자기 자신 제외 전체 가져오기 (유사도 계산용)
+    List<RoommateBoard> findAllByIdNot(Long boardId);
 
 }
