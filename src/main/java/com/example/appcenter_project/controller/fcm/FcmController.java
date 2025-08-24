c58d50ffa162b8668e152d0c3212dd6cc670f3b8
Jimin15 <sujean07015@gmail.com>
2025-08-24 17:24:45 +0900

    docs: FCM Swagger API 명세(FcmApiSpecification) 추가  #251

--- src/main/java/com/example/appcenter_project/controller/fcm/FcmController.java
+++ src/main/java/com/example/appcenter_project/controller/fcm/FcmController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/fcm")
 @RequiredArgsConstructor
-public class FcmController {
+public class FcmController implements FcmApiSpecification{
 
     private final FcmTokenService fcmTokenService;
     private final FcmMessageService fcmMessageService;
