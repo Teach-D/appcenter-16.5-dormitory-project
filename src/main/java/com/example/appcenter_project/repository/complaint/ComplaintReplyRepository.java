6623e66dbad24fba99b348d087d0bcfa14c19bdf
Jimin15 <sujean07015@gmail.com>
2025-08-29 06:12:29 +0900

    feat: 민원 답변(ComplaintReply) 리포지토리 생성 #270

---
+++ src/main/java/com/example/appcenter_project/repository/complaint/ComplaintReplyRepository.java
@@ -0,0 +1,7 @@
+package com.example.appcenter_project.repository.complaint;
+
+import com.example.appcenter_project.entity.complaint.ComplaintReply;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ComplaintReplyRepository extends JpaRepository<ComplaintReply, Long> {
+}
