29b34f6510f6a1c5757e1b052c1371c90e5f30bd
Teach-D <wkadht0619@gmail.com>
2025-09-28 13:23:32 +0900

    refactor: Role 필드명 변경 #315

--- src/main/java/com/example/appcenter_project/enums/user/Role.java
+++ src/main/java/com/example/appcenter_project/enums/user/Role.java
@@ -11,10 +11,10 @@ public enum Role {
 
     ROLE_ADMIN("관리자"),
     ROLE_USER("일반 사용자"),
-    DORM_MANAGER("기숙사 담당자"),
-    DORM_LIFE_MANAGER("기숙사 생활민원 담당자"),
-    DORM_ROOMMATE_MANAGER("기숙사 룸메이트민원 담당자"),
-    DORM_SUPPORTERS("기숙사 서포터즈");
+    ROLE_DORM_MANAGER("기숙사 담당자"),
+    ROLE_DORM_LIFE_MANAGER("기숙사 생활민원 담당자"),
+    ROLE_DORM_ROOMMATE_MANAGER("기숙사 룸메이트민원 담당자"),
+    ROLE_DORM_SUPPORTERS("기숙사 서포터즈");
 
     private final String description;
 
