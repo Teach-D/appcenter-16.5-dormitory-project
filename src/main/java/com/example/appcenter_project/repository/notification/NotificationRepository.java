2390e982ff3e2fd01babee6a117db525118beb03
Teach-D <wkadht0619@gmail.com>
2025-10-11 21:55:18 +0900

    add: 좋아요한 마감시간 임박한 공동구매 알림 #356

--- src/main/java/com/example/appcenter_project/repository/notification/NotificationRepository.java
+++ src/main/java/com/example/appcenter_project/repository/notification/NotificationRepository.java
@@ -1,9 +1,14 @@
 package com.example.appcenter_project.repository.notification;
 
 import com.example.appcenter_project.entity.notification.Notification;
+import com.example.appcenter_project.enums.user.NotificationType;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
     boolean existsByBoardIdAndTitle(Long boardId, String title);
     Notification findByBoardIdAndTitle(Long boardId, String title);
+
+    Notification findByBoardIdAndNotificationType(Long boardId, NotificationType notificationType);
 }
