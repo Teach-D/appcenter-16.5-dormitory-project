7c1b4306596975a269a4b720206bc38538a461e5
Teach-D <wkadht0619@gmail.com>
2025-10-11 21:32:01 +0900

    fix: complaint 제외한 알림 전송 생활원 직원 제외 #356

--- src/main/java/com/example/appcenter_project/repository/user/UserRepository.java
+++ src/main/java/com/example/appcenter_project/repository/user/UserRepository.java
@@ -4,7 +4,9 @@ import com.example.appcenter_project.entity.user.User;
 import com.example.appcenter_project.enums.user.DormType;
 import com.example.appcenter_project.enums.user.NotificationType;
 import com.example.appcenter_project.enums.user.Role;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -26,4 +28,22 @@ public interface UserRepository extends JpaRepository<User, Long> {
     List<User> findByDormTypeNotAndReceiveNotificationTypesContains(DormType dormType, NotificationType notificationType);
 
     List<User> findByReceiveNotificationTypesContains(NotificationType notificationType);
+
+    @Query("SELECT u FROM User u " +
+            "WHERE :notificationType MEMBER OF u.receiveNotificationTypes " +
+            "AND u.role NOT IN :excludedRoles")
+    List<User> findByReceiveNotificationTypesContainsAndRoleNotIn(
+            @Param("notificationType") NotificationType notificationType,
+            @Param("excludedRoles") List<Role> excludedRoles
+    );
+
+    @Query("SELECT u FROM User u " +
+            "WHERE u.dormType != :dormType " +
+            "AND :notificationType MEMBER OF u.receiveNotificationTypes " +
+            "AND u.role NOT IN :excludedRoles")
+    List<User> findByDormTypeNotAndReceiveNotificationTypesContainsAndRoleNotIn(
+            @Param("dormType") DormType dormType,
+            @Param("notificationType") NotificationType notificationType,
+            @Param("excludedRoles") List<Role> excludedRoles
+    );
 }
