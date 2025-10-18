4ef479bde14ca4bb6e9d6f320d9c678df043f800
Teach-D <wkadht0619@gmail.com>
2025-10-18 22:14:24 +0900

    fix: 쿠폰 발급 동시성 처리 및 dto 변경 #407

--- src/main/java/com/example/appcenter_project/repository/coupon/CouponRepository.java
+++ src/main/java/com/example/appcenter_project/repository/coupon/CouponRepository.java
@@ -2,10 +2,12 @@ package com.example.appcenter_project.repository.coupon;
 
 import com.example.appcenter_project.entity.coupon.Coupon;
 import jakarta.persistence.LockModeType;
-import org.apache.ibatis.annotations.Param;
+import jakarta.persistence.QueryHint;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
