970e2e4ed45f3c1f55e102a92aec4b33df39f4e7
Jimin15 <sujean07015@gmail.com>
2025-10-27 07:13:31 +0900

    feat: status enum 추가 #437

---
+++ src/main/java/com/example/appcenter_project/enums/survey/SurveyStatus.java
@@ -0,0 +1,32 @@
+package com.example.appcenter_project.enums.survey;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum SurveyStatus {
+    BEFORE("진행전"),      // 시작일 이전
+    PROCEEDING("진행중"),  // 진행 중
+    CLOSED("마감");        // 종료됨
+    
+    private final String description;
+    
+    @JsonCreator(mode = JsonCreator.Mode.DELEGATING)
+    public static SurveyStatus from(String value) {
+        for (SurveyStatus status : SurveyStatus.values()) {
+            if (status.getDescription().equals(value) || status.name().equalsIgnoreCase(value)) {
+                return status;
+            }
+        }
+        throw new IllegalArgumentException("Invalid SurveyStatus: " + value);
+    }
+    
+    @JsonValue
+    public String toValue() {
+        return this.description;
+    }
+}
+
