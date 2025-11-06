14b170692bdb0887ed8db8dff9ba4ea6ef2e864d
Teach-D <wkadht0619@gmail.com>
2025-11-06 11:04:01 +0900

    fix: findAllTipsIds @Query 추가 #345

--- src/main/java/com/example/appcenter_project/domain/tip/repository/TipRepository.java
+++ src/main/java/com/example/appcenter_project/domain/tip/repository/TipRepository.java
@@ -2,6 +2,7 @@ package com.example.appcenter_project.domain.tip.repository;
 
 import com.example.appcenter_project.domain.tip.entity.Tip;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,6 +11,8 @@ public interface TipRepository extends JpaRepository<Tip, Long> {
     Optional<Tip> findByIdAndUserId(Long id, Long userId);
     List<Tip> findAllByOrderByIdDesc();
     List<Tip> findByUserId(Long userId);
+
+    @Query("SELECT t.id FROM Tip t")
     List<Long> findAllTipIds();
     boolean existsByIdAndUserId(Long tipId, Long userId);
 }
