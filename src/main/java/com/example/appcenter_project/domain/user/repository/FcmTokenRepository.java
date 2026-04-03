8a525c66db913824effbc5db1002bad2944a46d6
Deach <110902175+Teach-D@users.noreply.github.com>
2026-04-03 13:03:08 +0900

    Merge: fix: FcmMessageService @Async 메서드 LazyInitializationException 수정 #579
    * chore: 이슈 #579 작업 시작
    
    * fix: @Async FCM 메서드 LazyInitializationException 수정 #579
    
    * chore: 이슈 #579 완료 처리

--- src/main/java/com/example/appcenter_project/domain/user/repository/FcmTokenRepository.java
+++ src/main/java/com/example/appcenter_project/domain/user/repository/FcmTokenRepository.java
@@ -4,12 +4,14 @@ import com.example.appcenter_project.domain.fcm.entity.FcmToken;
 import com.example.appcenter_project.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface FcmTokenRepository extends JpaRepository<FcmToken,Long> {
     void deleteByToken(String targetToken);
     boolean existsByToken(String token);
     Optional<FcmToken> findByUser(User user);
+    List<FcmToken> findAllByUser(User user);
 
     FcmToken findByToken(String token);
 }
